3e2ec4b46bf872bd239a5992a7d83ab63fa473b2
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-06-15 16:32:33 +0300

    add: interfaces

--- 04_oop/04_generics/Program.cs
+++ 04_oop/04_generics/Program.cs
@@ -124,18 +124,228 @@
 // Свойства:
 //  Длина хранилища
 
-class Container<T>
-{
-    private T[] arr;
-}
+//class Container<T>
+//{
+//    private T[] arr;
+//}
+
+#endregion
+
+
+#region Generics methods constraints
+
+////Archer a = new Archer(100);
+////Renderer.Render<Unit>(a);
+////Renderer.Render<Archer>(a);
 
 
 
+//Archer a = new Archer(100);
+//// Renderer.Render<int>(120);           // ERROR
+//Renderer.Render(a);
+
+//class Unit
+//{
+//    public int Hp { get; set; }
+//    public Unit(int hp) => Hp = hp;
+//}
+
+//class Warrior : Unit
+//{
+//    public Warrior(int hp) 
+//        : base(hp)
+//    {}
+//}
+
+//class Archer : Unit
+//{
+//    public Archer(int hp)
+//        : base(hp)
+//    { }
+//}
 
 
+//// ==== via downcasting                 :-|
+////class Renderer
+////{
+////    public static void Render<T>(T obj)
+////    {
+////        if (obj is Unit u)
+////            Console.WriteLine($"Renderer: {u.Hp}");
+////    }
+////}
 
+
+//// ==== via constraints                 :-)))
+//class Renderer
+//{
+//    public static void Render<T>(T obj)
+//        where T: Unit
+//    {
+//        Console.WriteLine($"Renderer: {obj.Hp}");
+//    }
+//}
+
+#endregion
+
+
+#region Genrics classes constraints
+
+//Renderer<Unit>.Render(new Warrior(120));
+//// Renderer<int>.Render(120);                  // ERROR
+
+//class Unit
+//{
+//    public int Hp { get; set; }
+//    public Unit(int hp) => Hp = hp;
+//}
+
+//class Warrior : Unit
+//{
+//    public Warrior(int hp)
+//        : base(hp)
+//    { }
+//}
+
+//class Archer : Unit
+//{
+//    public Archer(int hp)
+//        : base(hp)
+//    { }
+//}
+
+
+//class Renderer<T>
+//    where T: Unit
+//{
+//    public static void Render(T obj)
+//    {
+//        Console.WriteLine($"Renderer: {obj.Hp}");
+//    }
+//}
 
 
 #endregion
 
 
+#region Generics constraints types
+
+// where T: c1, c2, c3
+
+// -- c1: --
+// Конкретный класс
+//      или
+// class
+//      или
+// struct
+
+// -- с2: --
+// Конкретный интерфейс
+
+// -- с3: --
+// new()            // Тип T должен иметь публичный конструктор по-умолчанию
+
+
+//class Document
+//{
+//    public int Id { get; set; }
+//    public Document(int id)
+//    {
+//        Id = id;
+//    }
+//}
+
+//class Logger<T>
+//    // where T: Document
+//    // where T: class
+//    // where T: struct
+//    // where T: class, new()
+//    where T: new()
+//{
+//    void Execute()
+//    {
+//        T obj = new T();
+//    }
+//}
+
+//class Compressor<T, K>
+//    where T: class
+//    where K: Logger<int>
+//{
+
+//}
+
+#endregion
+
+
+#region Inheritance
+
+class Identity
+{
+    public Identity()
+    { }
+}
+class Unit<T>
+    where T: new()
+{
+    public T Id { get; set; }
+    public Unit(T id)
+    {
+        Id = id;
+    }
+}
+
+// --- 1. Обобщенный наследник с тем же типом
+
+//class Archer<K> : Unit<K>
+//    where K : new()
+//{
+//    public Archer(K id) 
+//        : base(id)
+//    {}
+//}
+
+// Unit<int> a = new Unit<int>(101);
+// Archer<int> b = new Archer<int>(102);
+// Unit<int> c = new Archer<int>(103);
+
+
+// --- 2. Обычный (необобщённый) наследник
+
+//class Warrior : Unit<Identity>
+//{
+//    public Warrior(Identity id) 
+//        : base(id)
+//    {}
+//}
+
+//Warrior a = new Warrior(new Identity());
+//Unit<int> b = new Unit<int>(101);
+
+
+// --- 3. Обобщённый наследник с собственным типом
+//class Archer<T> : Unit<int>
+//{
+//    public T? Value { get; set; }
+//    public Archer(int id) 
+//        : base(id)
+//    {}
+//}
+
+//Archer<string> a = new Archer<string>(140);
+
+
+// --- 4. Сочетание предыдущих вариантов
+
+//class Archer<T, K>: Unit<K>
+//    where K: new()
+//{
+//    public T? Value { get; set; }
+//    public Archer(K id) 
+//        : base(id)
+//    {}
+//}
+
+//Archer<int, Identity> a = new Archer<int, Identity>(new Identity());
+
+#endregion
---
+++ 04_oop/05_interfaces/Program.cs
@@ -0,0 +1,260 @@
+﻿
+#region Intro
+
+//interface IRenderable { }
+//interface IMovable { }
+//interface ILoggable { }
+//interface ILogger { }
+//interface IShape { }
+
+
+// Интерфейс может содержать
+// 1. Методы
+// 2. Свойства
+// 3. События
+// 4. Индексаторы
+// 5. Статика (константы)       // from C#8
+
+
+//internal interface ILogger
+//{
+//    // private int val;                 // ERROR
+//    public const double PI = 3.1415;
+//    public static int count = 0;
+//    public void Log(string message);
+//    public string Path { get; set; }
+
+//}
+
+
+
+
+// Shape a = new Shape();
+// a.Render();                 // ERROR
+
+//IRenderable b = new Shape();
+//b.Render();
+
+
+//interface IRenderable
+//{
+//    private static int count = 0;
+//    private const double PI = 3.14;
+//    private void Info()
+//    {
+//        Console.WriteLine("private Info()");
+//    }
+
+//    public void Render()
+//    {
+//        Console.WriteLine("Render");
+//        Console.WriteLine(count);
+//        Info();
+//    }
+
+//    public int Ratio { get => 0; }
+//    public double Factor => PI;
+//    public int Count
+//    {
+//        get => IRenderable.count;
+//        set => IRenderable.count = value < 0 ? 0 : value;
+//    }
+//}
+
+//class Shape: IRenderable
+//{
+
+//}
+
+#endregion
+
+
+#region Practice
+
+//Unit a = new Unit();
+//a.Render();
+
+//IRenderable b = new Unit();
+//b.Render();
+
+
+//IRenderable c = new Mesh();
+//c.WriteLogs();
+
+//IRenderable d = new Unit();
+//d.WriteLogs();
+
+//Mesh e = new Mesh();
+//// e.WriteLogs();                   ERROR
+
+
+//interface IRenderable
+//{
+//    public void Render();
+//    public void WriteLogs()
+//    {
+//        Console.WriteLine("IRenderable.WriteLogs()");
+//    }
+//}
+
+//class Unit : IRenderable
+//{
+//    public void Render()
+//    {
+//        Console.WriteLine("Unit.Render()");
+//    }
+
+//    public void WriteLogs()
+//    {
+//        Console.WriteLine("Unit.WriteLogs()");
+//    }
+//}
+
+//class Mesh : IRenderable
+//{
+//    public void Render()
+//    {
+//        Console.WriteLine("Mesh.Render()o");
+//    }
+//}
+
+#endregion
+
+
+#region Multiple implementation
+
+//Db db = new Db();
+
+//IWritable a = new Db();
+//a.Write();
+//IComparable b = new Db();
+//b.Compare(a);
+
+//if (db is IWritable iw)
+//    iw.Write();
+
+
+//interface IWritable
+//{
+//    public void Write();
+//}
+
+//interface IComparable
+//{
+//    public bool Compare(object? obj);
+//}
+
+//class Db : IWritable, IComparable
+//{
+//    public bool Compare(object? obj)
+//    {
+//        return true;
+//    }
+
+//    public void Write()
+//    {
+//        Console.WriteLine("Write to db...");
+//    }
+//    public void Save()
+//    {
+//        Console.WriteLine("Db.Save()");
+//    }
+//}
+
+#endregion
+
+
+#region Explicit realization (явная реализация)
+
+// неявная (классическая)
+//  когда методы интерфейса не конфликтуют
+//  может быть переопределена в классах-наследниках
+
+// явная
+//  когда есть явные конфликты и неоднозначности в именах / сигнатурах
+//  НЕ может быть переопределена в классах-наследниках
+
+
+
+// Db db = new Db();
+// db.Write();             // ERROR
+
+
+//IWritable a = new Db();
+//a.Write();
+
+//IWritable b = new FileManager();
+//b.Write();
+//FileManager c = new FileManager();
+//c.Write();
+
+//IWritable d = new Db();
+//d.Write();
+//ISaver e = new Db();
+//e.Write();
+
+
+
+Db db = new Db();
+
+((IWritable)db).Write();        // :-(
+
+if (db is IWritable wdb)        // :-)
+    wdb.Write();
+
+IWritable w = db;               // :-)
+w.Write();
+
+
+interface IWritable
+{
+    public void Write();
+}
+
+interface ISaver
+{
+    public void Write();
+}
+
+class Db: IWritable, ISaver
+{
+    // 1. Явная реализация
+    // 2. Не является членом класса!!!
+    // 3. Нельзя сделать public
+    void IWritable.Write()
+    {
+        Console.WriteLine("Write from Db(IWritable)");
+    }
+
+    void ISaver.Write()
+    {
+        Console.WriteLine("Write from Db(ISaver)");
+    }
+
+    // === общая реализация (одна на все интерфесы)
+    public void Write()
+    {
+        Console.WriteLine("Common realization");
+    }
+}
+
+class FileManager : IWritable
+{
+    void IWritable.Write()
+    {
+        Console.WriteLine("Write from FileManager(IWritable)");
+    }
+
+    public void Write()
+    {
+        Console.WriteLine("Write from FileManager");
+    }
+}
+
+
+
+
+
+
+
+#endregion
